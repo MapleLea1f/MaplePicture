c4839c196722d497201dd0f55f86f3ccd3f8dcc3
Maple <14208896+MapleLea1f@user.noreply.gitee.com>
2025-06-02 15:21:36 +0800

    动态分库分表实现（未测试）

---
+++ src/main/java/com/maple/maplepicturebackend/manager/sharding/DynamicShardingManager.java
@@ -0,0 +1,113 @@
+package com.maple.maplepicturebackend.manager.sharding;
+
+import com.maple.maplepicturebackend.model.entity.Space;
+import com.maple.maplepicturebackend.model.enums.SpaceTypeEnum;
+import com.maple.maplepicturebackend.service.SpaceService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.driver.jdbc.core.connection.ShardingSphereConnection;
+import org.apache.shardingsphere.infra.metadata.database.rule.ShardingSphereRuleMetaData;
+import org.apache.shardingsphere.mode.manager.ContextManager;
+import org.apache.shardingsphere.shadow.rule.ShadowRule;
+import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
+import org.apache.shardingsphere.sharding.api.config.rule.ShardingTableRuleConfiguration;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class DynamicShardingManager {
+
+    @Resource
+    private DataSource dataSource;
+
+    @Resource
+    private SpaceService spaceService;
+
+    private static final String LOGIC_TABLE_NAME = "picture";
+
+    private static final String DATABASE_NAME = "maple_picture"; // 配置文件中的数据库名称
+
+    public void initialize() {
+        log.info("初始化动态分表配置...");
+        updateShardingTableNodes();
+    }
+
+    /**
+     * 获取所有动态表名，包括初始化表 picture 和 分表 picture_{spaceId}
+     * @return
+     */
+    private Set<String> fetchAllPictureTableNames() {
+        // 测试，直接对所有团队空间分表（实际上线改为仅对旗舰版生效）
+        Set<Long> spaceIds = spaceService.lambdaQuery()
+                .eq(Space::getSpaceType, SpaceTypeEnum.TEAM.getValue())
+                .list()
+                .stream()
+                .map(Space::getId)
+                .collect(Collectors.toSet());
+        Set<String> tableNames = spaceIds.stream()
+                .map(spaceId -> LOGIC_TABLE_NAME + "_" + spaceId)
+                .collect(Collectors.toSet());
+        tableNames.add(LOGIC_TABLE_NAME); // 添加初始逻辑表
+        return tableNames;
+    }
+
+    /**
+     * 更新 ShardingSphere 的 actual-data-nodes 动态表名配置
+     */
+    private void updateShardingTableNodes() {
+        Set<String> tableNames = fetchAllPictureTableNames();
+        String newActualDataNodes = tableNames.stream()
+                .map(tableName -> "maple_picture." + tableName) // 确保前缀合法
+                .collect(Collectors.joining(","));
+        log.info("动态分表 actual-data-nodes 配置:{}", newActualDataNodes);
+
+        ContextManager contextManager = getContextManager();
+        ShardingSphereRuleMetaData ruleMetaData = contextManager.getMetaDataContexts()
+                .getMetaData()
+                .getDatabases()
+                .get(DATABASE_NAME)
+                .getRuleMetaData();
+
+        Optional<ShadowRule> shardingRule = ruleMetaData.findSingleRule(ShadowRule.class);
+        if (shardingRule.isPresent()) {
+            ShardingRuleConfiguration ruleConfig = (ShardingRuleConfiguration) shardingRule.get().getConfiguration();
+            List<ShardingTableRuleConfiguration> updateRules = ruleConfig.getTables()
+                    .stream()
+                    .map(oldTableRule -> {
+                        if (LOGIC_TABLE_NAME.equals(oldTableRule.getLogicTable())){
+                            ShardingTableRuleConfiguration newTableRuleConfig = new ShardingTableRuleConfiguration(LOGIC_TABLE_NAME, newActualDataNodes);
+                            newTableRuleConfig.setDatabaseShardingStrategy(oldTableRule.getDatabaseShardingStrategy());
+                            newTableRuleConfig.setTableShardingStrategy(oldTableRule.getTableShardingStrategy());
+                            newTableRuleConfig.setKeyGenerateStrategy(oldTableRule.getKeyGenerateStrategy());
+                            newTableRuleConfig.setAuditStrategy(oldTableRule.getAuditStrategy());
+                            return newTableRuleConfig;
+                        }
+                        return oldTableRule;
+                    })
+                    .collect(Collectors.toList());
+            ruleConfig.setTables(updateRules);
+            contextManager.alterRuleConfiguration(DATABASE_NAME, Collections.singleton(ruleConfig));
+            contextManager.reloadDatabase(DATABASE_NAME);
+            log.info("动态分表规则更新成功");
+        } else {
+            log.error("未找到 ShardingSphere的分片规则配置,动态分表更新失败");
+        }
+
+    }
+
+    /**
+     * 获取Sharding ContextManager
+     */
+    private ContextManager getContextManager() {
+        try(ShardingSphereConnection connection = dataSource.getConnection().unwrap(ShardingSphereConnection.class)) {
+            return connection.getContextManager();
+        } catch (SQLException e) {
+            throw new RuntimeException("获取 ShardingSphere ContextManager 失败", e);
+        }
+    }
+}
